33d425c1403389ac566833cf346e0f9b401bd369
Piotr Piotrowski <piotr@piotrovski.com>
2017-03-28 14:21:40 +0200

    initial layout

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Piotr Piotrowski on 24/03/2017.
+//  Copyright © 2017 Piotr Piotrowski. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
