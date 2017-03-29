9277e6170a96b3296adeb59fe0dda71494d859ca
Piotr Piotrowski <piotr@piotrovski.com>
2017-03-29 11:56:31 +0200

    Lecture 64. iOS 10 App: Party Rock - Downloading images & async

--- PartyRockApp/PartyCell.swift
+++ PartyRockApp/PartyCell.swift
@@ -22,7 +22,18 @@ class PartyCell: UITableViewCell {
     func updateUI(partRock: PartyRock) {
         videoTitle.text = partRock.videoTitle
         
+        let url = URL(string: partRock.imageURL)!
         
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            } catch {
+                // handle the error
+            }
+        }
     }
     
 }
